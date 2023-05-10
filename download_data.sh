6ec303abaf08a3c96c1c9c50bf4d78097219bbda
Julius Rückin <julius.rueckin@gmail.com>
2023-05-10 10:33:22 +0200

    update data download script

--- download_data.sh
+++ download_data.sh
@@ -1,13 +1,13 @@
-curl -LO https://phenoroam.phenorob.de/geonetwork/srv/api/records/be36e0bc-bb15-4e7c-b341-5754670ff29c/attachments/worlds.zip
-curl -LO https://phenoroam.phenorob.de/geonetwork/srv/api/records/be36e0bc-bb15-4e7c-b341-5754670ff29c/attachments/flightmare.zip
-curl -LO https://phenoroam.phenorob.de/geonetwork/srv/api/records/be36e0bc-bb15-4e7c-b341-5754670ff29c/attachments/potsdam.zip
-curl -LO https://phenoroam.phenorob.de/geonetwork/srv/api/records/be36e0bc-bb15-4e7c-b341-5754670ff29c/attachments/rit18.zip
-curl -LO https://phenoroam.phenorob.de/geonetwork/srv/api/records/be36e0bc-bb15-4e7c-b341-5754670ff29c/attachments/pretrained.zip
+curl -LO https://phenoroam.phenorob.de/file-uploader/download/public/6314864478-worlds.zip
+curl -LO https://phenoroam.phenorob.de/file-uploader/download/public/1878135163-flightmare.zip
+curl -LO https://phenoroam.phenorob.de/file-uploader/download/public/4930684482-potsdam.zip
+curl -LO https://phenoroam.phenorob.de/file-uploader/download/public/2463968057-rit18.zip
+curl -LO https://phenoroam.phenorob.de/file-uploader/download/public/244704144-pretrained.zip
 
-unzip worlds.zip -d .
-unzip flightmare.zip -d bayesian_erfnet/agri_semantics/datasets/
-unzip potsdam.zip -d bayesian_erfnet/agri_semantics/datasets/
-unzip rit18.zip -d bayesian_erfnet/agri_semantics/datasets/
-unzip pretrained.zip -d bayesian_erfnet/agri_semantics/models/
+unzip 6314864478-worlds.zip -d .
+unzip 1878135163-flightmare.zip -d bayesian_erfnet/agri_semantics/datasets/
+unzip 4930684482-potsdam.zip -d bayesian_erfnet/agri_semantics/datasets/
+unzip 2463968057-rit18.zip -d bayesian_erfnet/agri_semantics/datasets/
+unzip 244704144-pretrained.zip -d bayesian_erfnet/agri_semantics/models/
 
-rm worlds.zip flightmare.zip potsdam.zip rit18.zip pretrained.zip
+rm 6314864478-worlds.zip 1878135163-flightmare.zip 4930684482-potsdam.zip 2463968057-rit18.zip 244704144-pretrained.zip
